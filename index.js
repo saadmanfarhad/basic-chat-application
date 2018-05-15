5d8b006005a5e5661e429ca1b4e23977a914f718
saadmanfarhad <saadmanheavy@gmail.com>
2018-05-16 00:19:12 +0600

    Completed express and socket.io setup

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var socket = require('socket.io');
+
+//App Setup
+var app = express();
+var server = app.listen(4000,function () {
+  console.log('Listening on port 4000');
+});
+
+//Static Files
+app.use(express.static('public'));
+
+//Socket Setup
+var io = socket(server);
+
+io.on('connection', function (socket) {
+  console.log('Made socket connection');
+});
